13b905ffc331f979856fb8cad8029dd4ef9b16d5
edwin <edwin.lzh@gmail.com>
2017-08-29 19:02:03 +0800

    fix

---
+++ vendor/github.com/qiniu/api.v7/conf/conf.go
@@ -0,0 +1,29 @@
+package conf
+
+import (
+	"fmt"
+	"github.com/qiniu/x/ctype.v7"
+	"github.com/qiniu/x/rpc.v7"
+	"runtime"
+	"syscall"
+)
+
+var version = "7.2.0"
+
+const (
+	ctypeAppName = ctype.ALPHA | ctype.DIGIT | ctype.UNDERLINE | ctype.SPACE_BAR | ctype.SUB | ctype.DOT
+)
+
+// userApp should be [A-Za-z0-9_\ \-\.]*
+func SetAppName(userApp string) error {
+	if userApp != "" && !ctype.IsType(ctypeAppName, userApp) {
+		return syscall.EINVAL
+	}
+	rpc.UserAgent = fmt.Sprintf(
+		"QiniuGo/%s (%s; %s; %s) %s", version, runtime.GOOS, runtime.GOARCH, userApp, runtime.Version())
+	return nil
+}
+
+func init() {
+	SetAppName("")
+}
