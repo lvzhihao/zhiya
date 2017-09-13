c16b2e02877734ba1ee56f9cb35652a4afc58599
edwin <edwin.lzh@gmail.com>
2017-09-13 21:22:31 +0800

    fix

--- utils/queue.go
+++ utils/queue.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func RegisterQueue(api, user, passwd, vhost, name, exchange, key string) error {
+func CreateQueue(api, user, passwd, vhost, name string) error {
 	client := &http.Client{}
 	b := bytes.NewBufferString(`{"auto_delete":false, "durable":true, "arguments":[]}`)
 	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/queues/%s/%s", api, vhost, name), b)
@@ -23,22 +23,36 @@ func RegisterQueue(api, user, passwd, vhost, name, exchange, key string) error {
 	if resp.StatusCode != http.StatusNoContent {
 		return fmt.Errorf("CreateQueue StatusError: %d, %v", resp.StatusCode, resp)
 	}
+	return nil
+}
+
+func BindRoutingKey(api, user, passwd, vhost, name, exchange, key string) error {
+	client := &http.Client{}
+	b := bytes.NewBufferString(`{"routing_key":"` + key + `", "arguments":[]}`)
+	// ensure binding
+	req, err := http.NewRequest(
+		"POST",
+		fmt.Sprintf("%s/bindings/%s/e/%s/q/%s", api, vhost, exchange, name),
+		b)
+	req.SetBasicAuth(user, passwd)
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusCreated {
+		return fmt.Errorf("BindRoutingKey StatusError: %d, %v", resp.StatusCode, resp)
+	}
+	return nil
+}
+
+func RegisterQueue(api, user, passwd, vhost, name, exchange, key string) error {
+	err := CreateQueue(api, user, passwd, vhost, name)
+	if err != nil {
+		return err
+	}
 	if exchange != "" && key != "" {
-		b = bytes.NewBufferString(`{"routing_key":"` + key + `", "arguments":[]}`)
-		// ensure binding
-		req, err = http.NewRequest(
-			"POST",
-			fmt.Sprintf("%s/bindings/%s/e/%s/q/%s", api, vhost, exchange, name),
-			b)
-		req.SetBasicAuth(user, passwd)
-		req.Header.Add("Content-Type", "application/json")
-		resp, err = client.Do(req)
-		if err != nil {
-			return err
-		}
-		if resp.StatusCode != http.StatusCreated {
-			return fmt.Errorf("BindRoutingKey StatusError: %d, %v", resp.StatusCode, resp)
-		}
+		return BindRoutingKey(api, user, passwd, vhost, name, exchange, key)
 	}
 	return nil
 }
