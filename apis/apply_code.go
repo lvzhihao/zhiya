f4c9a97b73c7c5476865a37324788b627f47d492
edwin <edwin.lzh@gmail.com>
2018-01-10 17:31:48 +0800

    fix

--- apis/apply_code.go
+++ apis/apply_code.go
@@ -66,7 +66,7 @@ func FetchApplyCode(db *gorm.DB, myId, subId, robotSerialNo, limit string) (*mod
 	}
 	// todo check subid chatroom limit
 
-	limitNum := 30
+	limitNum := 10
 	if limit != "" {
 		limitNum = int(goutils.ToInt(limit))
 	}
